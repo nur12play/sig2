581dc99b44e8a2fa0071612a4535270e0b1efd04
nur12play <140480965+nur12play@users.noreply.github.com>
2025-04-13 20:45:00 +0500

    1 stage

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,37 @@
 public class Main {
     public static void main(String[] args) {
         MyArrayList<Integer> arrayList = new MyArrayList<>();
-
-        System.out.println("Is ArrayList empty? " + arrayList.isEmpty());
-
         arrayList.add(10);
         arrayList.add(20);
         arrayList.add(30);
+        arrayList.addFirst(5);
+        arrayList.addLast(40);
+        System.out.println("ArrayList: ");
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
 
-        System.out.println("Is ArrayList empty? " + arrayList.isEmpty());
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println("Stack: ");
+        System.out.println(stack.pop());  // 30
+        System.out.println(stack.peek()); // 20
 
-        arrayList.remove(0);
-        arrayList.remove(0);
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        System.out.println("Queue: ");
+        System.out.println(queue.dequeue()); // 10
+        System.out.println(queue.dequeue()); // 20
 
-        System.out.println("Is ArrayList empty? " + arrayList.isEmpty());
-
-        arrayList.remove(0);
-
-        System.out.println("Is ArrayList empty? " + arrayList.isEmpty());
+        MyMinHeap<Integer> minHeap = new MyMinHeap<>();
+        minHeap.insert(30);
+        minHeap.insert(10);
+        minHeap.insert(20);
+        System.out.println("MinHeap: ");
+        System.out.println(minHeap.extractMin()); // 10
     }
 }
